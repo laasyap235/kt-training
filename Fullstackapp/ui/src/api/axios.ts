75ff1ec813a4c07ec2472516ec685b7803ba6039
laasyap235 <laasyakshara.p@waferwire.com>
2026-06-22 15:47:26 +0530

    Change API base URL to production server

--- Fullstackapp/ui/src/api/axios.ts
+++ Fullstackapp/ui/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5096/api',
+  baseURL: 'https://newwebapp-ddahc5atayhchzaa.eastasia-01.azurewebsites.net/api',
 })
 
 export default api
